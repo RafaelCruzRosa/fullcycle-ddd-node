44deef49c47ac5bfe9c7e3c2e7f4d61d5c808801
Rafael Cruz Rosa <rafaelcruzrosa1@gmail.com>
2022-09-01 22:33:25 -0300

    feat: customer repository find method

--- src/infrastructure/repository/customer-repository.ts
+++ src/infrastructure/repository/customer-repository.ts
@@ -1,4 +1,5 @@
 import { Customer } from "../../domain/entity/customer";
+import Address from "../../domain/entity/valueObjects/address";
 import RepositoryInterface from "../../domain/repository/repository-interface";
 import CustomerModel from "../db/sequelize/model/customer.model";
 
@@ -22,8 +23,19 @@ export default class CustomerRepository implements RepositoryInterface<Customer>
   update(entity: Customer): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  find(id: string): Promise<Customer> {
-    throw new Error("Method not implemented.");
+  async find(id: string): Promise<Customer> {
+    try {
+      const customer = await CustomerModel.findOne({ where: { id } })
+
+      if (!customer) {
+        throw new Error("Customer not found")
+      }
+
+      const customerAddress = new Address(customer.street, customer.number, customer.zip, customer.city);
+      return new Customer(customer.id, customer.name, customer.email, customerAddress)
+    } catch (error) {
+      throw error
+    }
   }
   findAll(): Promise<Customer[]> {
     throw new Error("Method not implemented.");
