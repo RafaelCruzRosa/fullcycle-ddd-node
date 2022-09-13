991efcfc75cbb75c120789748f720efb1c10119d
Rafael Cruz Rosa <rafaelcruzrosa1@gmail.com>
2022-09-12 21:47:41 -0300

    test: order repository find method test

--- src/infrastructure/repository/order-repository.spec.ts
+++ src/infrastructure/repository/order-repository.spec.ts
@@ -66,7 +66,60 @@ describe("Order Repository test", () => {
         quantity: orderItem.quantity
       }]
     })
+  })
 
+  it("should be able to find and order", async () => {
+    const productRepository = new ProductRepository()
+    const product = new Product("1", "product name", 100)
+    await productRepository.create(product)
+
+    const customerRepository = new CustomerRepository()
+    const customer = new Customer("1", "customer name", "customer@customer.com", new Address("street", 1, "123", "city"))
+    await customerRepository.create(customer)
+
+    const orderItem = new OrderItem("1", "order item", 10, product.id, 2)
+
+    const order = new Order("1", customer.id, [orderItem])
+    const orderRepository = new OrderRepository()
+    await orderRepository.create(order)
+
+    const orderModelFinded = await OrderModel.findOne({ where: { id: order.id }, include: ["items"] })
+    const orderFinded = await orderRepository.find(order.id)
+
+    if (!orderModelFinded) {
+      expect(true).toBeFalsy()
+      throw new Error("Error in test")
+    }
+
+    expect({
+      id: orderFinded.id,
+      customerId: orderFinded.customerId,
+      items: [{
+        id: orderItem.id,
+        name: orderItem.name,
+        price: orderItem.price,
+        productId: orderItem.productId,
+        quantity: orderItem.quantity
+      }]
+    }).toStrictEqual({
+      id: orderModelFinded.id,
+      customerId: orderModelFinded.customer_id,
+      items: [{
+        id: orderItem.id,
+        name: orderItem.name,
+        price: orderItem.price,
+        productId: orderItem.productId,
+        quantity: orderItem.quantity
+      }]
+    })
+
+  })
+  it("should throw if order not found", async () => {
+    const orderRepository = new OrderRepository()
+
+    expect(async () => {
+      await orderRepository.find("ABC")
+    }).rejects.toThrow("Order not found")
 
   })
 })
